a5e663fe13972e7cd01d2a12b3c8e6f2585b1c76
Slyker <ramzy694@gmail.com>
2024-05-02 18:44:08 -0400

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use image::{ImageBuffer, Rgba};
-use image_analyzer::{pixel::PixelVec, ImageZone};
+use image_analyzer::{
+    color::{hsv::Hsv, rgb::Rgb, Color},
+    pixel::PixelVec,
+    ImageZone,
+};
 use utils::benchmark::Benchmark;
 
 mod data;
@@ -11,36 +15,10 @@ const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 fn main() {
     let image: ImageBuffer<Rgba<u8>, Vec<u8>> = image::ImageBuffer::new(WIDTH, HEIGHT);
-
-    let mut benchmark = Benchmark::new(BENCH_ITER, "bench_par_detect_v1", true);
-    benchmark.run(|i| {
-        let px_vec = bench_par_detect_v1(image.clone());
-        if i == BENCH_ITER - 1 {
-            println!(
-                "Size: {}, Total: {}",
-                px_vec.pixels.len(),
-                px_vec.points_count
-            );
-        }
-    });
-    println!("------------------------------------------");
-
-    let mut benchmark = Benchmark::new(BENCH_ITER, "bench_detect_v1", true);
-    benchmark.run(|i| {
-        let px_vec = bench_detect_v1(image.clone());
-        if i == BENCH_ITER - 1 {
-            println!(
-                "Size: {}, Total: {}",
-                px_vec.pixels.len(),
-                px_vec.points_count
-            );
-        }
-    });
-    println!("------------------------------------------");
-
     let mut benchmark = Benchmark::new(BENCH_ITER, "bench_detect_v2", true);
     benchmark.run(|i| {
-        let px_vec = bench_detect_v2(image.clone());
+        let mut analyzer = image_analyzer::ImageAnalyzer::new(image.clone());
+        let px_vec = bench_detect_v2(&mut analyzer);
         if i == BENCH_ITER - 1 {
             println!(
                 "Size: {}, Total: {}",
@@ -53,27 +31,12 @@ fn main() {
     println!("Done image size: {}x{}", WIDTH, HEIGHT);
 }
 
-fn bench_detect_v1(image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> PixelVec {
-    let mut points = PixelVec::new();
-    let analyzer = image_analyzer::ImageAnalyzer::new(image);
-    analyzer.pixel_detectv1(|color, point| {
-        points.push((color, point));
-        None
-    });
-    points
-}
+fn bench_detect_v2(analyzer: &mut image_analyzer::ImageAnalyzer) -> &PixelVec {
+    //analyzer.pixel_detectv2(ImageZone::Full)
 
-fn bench_detect_v2(image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> PixelVec {
-    let mut points = PixelVec::new();
-    let analyzer = image_analyzer::ImageAnalyzer::new(image);
-    analyzer.pixel_detectv2(ImageZone::Full, |color, point| {
-        points.push((color, point));
-        None
-    });
-    points
-}
-fn bench_par_detect_v1(image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> PixelVec {
-    let analyzer = image_analyzer::ImageAnalyzer::new(image);
-    let res = analyzer.par_pixel_detectv1();
-    res
+    analyzer.detect_pixel_with_tolerance(
+        ImageZone::Full,
+        Color::Rgb(Rgb::from([0, 0, 0, 0])),
+        Color::Hsv(Hsv::from([0.5, 0.5, 0.5, 0.5])),
+    )
 }
